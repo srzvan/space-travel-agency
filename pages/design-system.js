de0a1fa51c6542f3e1485c38c354785abbe38251
srzvan <sbingu.razvan@gmail.com>
2021-11-04 21:47:21 +0200

    typography section of the design system page

--- pages/design-system.js
+++ pages/design-system.js
@@ -4,9 +4,11 @@ import styles from "styles/pages/DesignSystem.module.css";
 
 export default function DesignSystem() {
   return (
-    <div className="container text-white">
-      <h1 className="ff-sans-cond uppercase">Design system</h1>
-      <section id="colors">
+    <div className="container text-white" style={{ padding: "4rem 0" }}>
+      <h1 className="ff-sans-cond uppercase" style={{ marginBottom: "6.25rem" }}>
+        Design system
+      </h1>
+      <section id="colors" style={{ marginBottom: "9.25rem" }}>
         <h2 className="ff-sans-cond fs-500 uppercase letter-spacing-1">
           <span className={styles.number}>01</span> colors
         </h2>
@@ -51,6 +53,61 @@ export default function DesignSystem() {
         <h2 className="ff-sans-cond fs-500 uppercase letter-spacing-1">
           <span className={styles.number}>02</span> typography
         </h2>
+        <div className="flex">
+          <div style={{ flexBasis: "100%" }}>
+            <div>
+              <p className="text-accent">Heading 1 - Bellefair Regular - 150px</p>
+              <p className="ff-serif fs-900 uppercase">Earth</p>
+            </div>
+            <div>
+              <p className="text-accent">Heading 2 - Bellefair Regular - 100px</p>
+              <p className="ff-serif fs-800 uppercase">Venus</p>
+            </div>
+            <div>
+              <p className="text-accent">Heading 3 - Bellefair Regular - 56px</p>
+              <p className="ff-serif fs-700 uppercase">Jupiter & Saturn</p>
+            </div>
+            <div>
+              <p className="text-accent">Heading 4 - Bellefair Regular - 32px</p>
+              <p className="ff-serif fs-600 uppercase">Uranus, Neptune, & Pluto</p>
+            </div>
+            <div>
+              <p className="text-accent">Heading 5 - Barlow Condensed Regular - 28px - 4.75 Character Space</p>
+              <p className="ff-sans-cond fs-500 uppercase letter-spacing-1 text-accent">
+                So, you want to travel to space
+              </p>
+            </div>
+          </div>
+
+          <div style={{ flexBasis: "100%" }}>
+            <div>
+              <p className="text-accent">Subheading 1 - Bellefair Regular - 28px</p>
+              <p className="fs-500 ff-serif">384,400 km</p>
+            </div>
+            <div>
+              <p className="text-accent">Subheading 2 - Barlow Condensed Regular - 14px - 2.35 Character Space</p>
+              <p className="fs-200 ff-sans-cond letter-spacing-3 uppercase">Avg. Distance</p>
+            </div>
+            <div>
+              <p className="text-accent">Nav Text - Barlow Condensed Regular - 16px - 2.7 Character Space</p>
+              <p className="fs-300 ff-sans-cond letter-spacing-2 uppercase">Europa</p>
+            </div>
+            <div>
+              <p className="text-accent-faded">Body Text</p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit. Pellentesque aliquet nibh
+                nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed pretium, ligula sollicitudin
+                laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu nibh. Nullam mollis. Ut justo.
+                Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus hendrerit.
+                Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id, mattis vel, nisi. Sed
+                pretium, ligula sollicitudin laoreet viverra, tortor libero sodales leo, eget blandit nunc tortor eu
+                nibh. Nullam mollis. Ut justo. Suspendisse potenti.Lorem ipsum dolor sit amet, consectetuer adipiscing
+                elit. Phasellus hendrerit. Pellentesque aliquet nibh nec urna. In nisi neque, aliquet vel, dapibus id,
+                mattis vel, nisi.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
