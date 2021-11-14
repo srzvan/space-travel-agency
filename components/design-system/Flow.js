914b06a05d35d33f85dbb9dfe5c8b4017c1aa486
srzvan <sbingu.razvan@gmail.com>
2021-11-14 12:44:37 +0200

    set spacing css var correctly in <Flow/>

--- components/design-system/Flow.js
+++ components/design-system/Flow.js
@@ -1,11 +1,9 @@
 export default function Flow(props) {
   const { children, className, style, spacing } = props;
+  const withSpacing = spacing && { "--flow-space": `${spacing}` };
 
   return (
-    <div
-      className={className ? `${className} flow` : "flow"}
-      style={{ ...style, ...(spacing && { "--flow-space": `${spacing}` }) }}
-    >
+    <div className={className ? `${className} flow` : "flow"} style={{ ...style, ...withSpacing }}>
       {children}
     </div>
   );
