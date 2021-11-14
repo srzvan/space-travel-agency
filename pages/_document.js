b05bc1be60e7a7babd8e57311d61aa8c1bb542e1
srzvan <sbingu.razvan@gmail.com>
2021-11-14 19:24:43 +0200

    WIP: homepage

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="icon" type="image/png" sizes="32x32" href="./favicon.ico" />
+
           {/* Google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
 export default function Home() {
   return (
-    <main>
-      <h1>This is the index page</h1>
-    </main>
+    <div className="container">
+      <header>
+        <nav>
+          <ul className="fs-300 ff-sans-cond letter-spacing-2 text-white uppercase flex underline-indicators primary-navigation">
+            <li>
+              <a href="#">00 Home</a>
+            </li>
+            <li>
+              <a href="#">01 Destination</a>
+            </li>
+            <li>
+              <a href="#">02 Crew</a>
+            </li>
+            <li>
+              <a href="#">03 Technology</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <section className="flex" style={{ justifyContent: "space-around" }}>
+          <article style={{ flexBasis: 0 }}>
+            <p className="ff-sans-cond fs-500 uppercase letter-spacing-1 text-accent">So, you want to travel to</p>
+            <p className="ff-serif fs-900 uppercase" style={{ lineHeight: 1.146 }}>
+              Space
+            </p>
+            <p className="ff-sans-normal fs-400 text-accent">
+              Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover
+              kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world
+              experience!
+            </p>
+          </article>
+          <button
+            className="ff-serif fs-600 uppercase text-dark bg-white letter-spacing-3 large-button"
+            style={{ flexBasis: 0 }}
+          >
+            Explore
+          </button>
+        </section>
+      </main>
+    </div>
   );
 }
