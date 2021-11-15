ae830dcb14d24596cf92f911a021e3682a151fdd
srzvan <sbingu.razvan@gmail.com>
2021-11-15 18:27:24 +0200

    move responsive meta tag in _app

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,11 @@ class CustomDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="icon" type="image/png" sizes="32x32" href="./favicon.ico" />
 
           {/* Google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="annonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@400;700&family=Bellefair&family=Barlow:wght@400;700&display=swap"
             rel="stylesheet"
